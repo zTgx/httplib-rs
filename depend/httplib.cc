fb33f415ddb141d9b49654aeae35b19a5c8e3b7d
zTgx <747674262@qq.com>
2019-08-13 22:13:52 +0800

    eliminate warning: warning: 'make_client' has C-linkage specified, but returns incomplete type 'std::shared_ptr<httplib::Client>' which could be incompatible with C [-Wreturn-type-c-linkage]

--- depend/httplib.cc
+++ depend/httplib.cc
@@ -1,8 +1,12 @@
 #include "cpp-httplib/httplib.h"
 #include <memory>
 
+#if defined(__clang__)
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#endif
 
 extern "C" {
     std::shared_ptr<httplib::Client> make_client(const char * host, int port = 80, time_t timeout_sec = 300) {
@@ -11,4 +15,7 @@ extern "C" {
     }
 }
 
+#if defined(__clang__)
 #pragma clang diagnostic pop
+#pragma clang diagnostic pop
+#endif
