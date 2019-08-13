acf2e8e9e789509f4f603cedd2ecffda50ed6846
beautifularea <beautifularea@gmail.com>
2019-08-13 19:49:11 +0800

    create client build successfully.

--- depend/httplib.cc
+++ depend/httplib.cc
@@ -1,10 +1,10 @@
 #include "cpp-httplib/httplib.h"
-#include <string>
+#include <memory>
 
 extern "C" {
-    Client* create_client(std::string host, int port = 80, time_t timeout_sec = 300) {
+    std::shared_ptr<httplib::Client> create_client(const char * host, int port = 80, time_t timeout_sec = 300) {
         // Client *client = new(Client(host, port));
-        std::shared<Client> client = std::make_shared( Client(host, port) );
+        std::shared_ptr<httplib::Client> client = std::make_shared<httplib::Client>( host, port, timeout_sec );
 
         return client;
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 extern crate libc;
 
 pub mod ffi;
+use crate::ffi::create_client;
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug, PartialOrd, Ord, Hash)]
 pub struct Client(ffi::Client);
 
 impl Client {
     pub fn new(host: String, port: isize, time_t: i64) -> Self {
-        Client {
-            create_client,
-        }
+        Client (
+            unsafe { create_client(host.as_ptr() as *const i8, port as libc::c_int, time_t as libc::time_t) }
+        )
     }
 }
