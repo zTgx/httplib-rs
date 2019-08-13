4f2e965635c61c185e1e6cfd833639dac17ac502
zTgx <747674262@qq.com>
2019-08-13 18:24:45 +0800

    update build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,36 @@
+//! # Build script
+
+// Coding conventions
+#![deny(non_upper_case_globals)]
+#![deny(non_camel_case_types)]
+#![deny(non_snake_case)]
+#![deny(unused_mut)]
+#![warn(missing_docs)]
+
+extern crate cc;
+
+use std::env;
+
 fn main() {
-    
+    // Check whether we can use 64-bit compilation
+    let _use_64bit_compilation = if env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "64" {
+        let check = cc::Build::new().file("depend/check_uint128_t.c")
+                                    .cargo_metadata(false)
+                                    .try_compile("check_uint128_t")
+                                    .is_ok();
+        if !check {
+            println!("cargo:warning=Compiling in 32-bit mode on a 64-bit architecture due to lack of uint128_t support.");
+        }
+        check
+    } else {
+        false
+    };
+
+    // Actual build cpp-httplib
+    cc::Build::new()
+        .file("depend/httplib.cc")
+        .cpp(true)
+        .flag("-std=c++11")
+        .cpp_link_stdlib("stdc++")
+        .compile("libhttp.a");
 }
---
+++ depend/httplib.cc
@@ -0,0 +1,1 @@
+#include "cpp-httplib/httplib.h"
